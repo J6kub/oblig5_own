c951ee0b2245478f128966846957c776aaa1f4e8
J6kub <jakubb@uia.no>
2024-11-04 15:30:31 +0100

    Lagt til nytt barnehage

--- init_db.py
+++ init_db.py
@@ -25,6 +25,7 @@ def init_db(print_progress=False):
         tbl_barnehager.rowAppend("3;Kakerlakker Hage;250;120")
         tbl_barnehager.rowAppend("4;Barnepoliti skolen;100;100")
         tbl_barnehager.rowAppend("5;Abc 123 hagebarn;101;102")
+        tbl_barnehager.rowAppend("6;Barnehage for katter;10;0")
         tbl_barnehager.save()
     except ValueError:
         tbl_barnehager = TableCsv(db_path + "DB_barnehager.csv",splitter,progress=print_progress)
